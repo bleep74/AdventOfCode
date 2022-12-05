989c2abfaa1ebbaea580b4676c7e4ac773d6374c
Corey <bleep74@yahoo.com>
2022-12-05 11:29:10 -0600

    Add Day 5

---
+++ Day5/Task1/crates.py
@@ -0,0 +1,37 @@
+#python script
+#Day 5 Challenge 1
+import time
+
+tstart = time.time()
+file1 = open('elves.txt','r')
+
+crates = ["","","","","","","","",""]
+
+crates[0] = ["S","M","R","N","W","J","V","T"]
+crates[1] = ["B","W","D","J","Q","P","C","V"]
+crates[2] = ["B","J","F","H","D","R","P"]
+crates[3] = ["F","R","P","B","M","N","D"]
+crates[4] = ["H","V","R","P","T","B"]
+crates[5] = ["C","B","P","T"]
+crates[6] = ["B","J","R","P","L"]
+crates[7] = ["N","C","S","L","T","Z","B","W"]
+crates[8] = ["L","S","G"]
+
+Lines = file1.readlines()
+
+for line in Lines:
+	motions = line.rstrip().split(" ")
+	count = 0
+	
+	while count < int(motions[1]):
+		letter = crates[int(motions[3])-1][len(crates[int(motions[3])-1]) - 1]
+		crates[int(motions[3])-1].pop(len(crates[int(motions[3])-1]) - 1)
+		crates[int(motions[5])-1].append(letter)
+		count +=1
+
+
+		
+print("Final Letters : {} {} {} {} {} {} {} {} {}".format(crates[0][len(crates[0]) - 1],crates[1][len(crates[1]) - 1],crates[2][len(crates[2]) - 1],crates[3][len(crates[3]) - 1], crates[4][len(crates[4]) - 1], crates[5][len(crates[5]) - 1], crates[6][len(crates[6]) - 1], crates[7][len(crates[7]) - 1],crates[8][len(crates[8]) - 1]))
+		
+tstop = (time.time() - tstart)
+print ("Time : {}".format(tstop))
